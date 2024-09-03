2fcd9aa6271eea00d8c8ab6d7426eedf8df537ce
kamack38 <64226248+kamack38@users.noreply.github.com>
2024-09-03 14:15:17 +0200

    feat: 🎸 Send webhook when a new exam is found

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use chrono::{Days, Utc};
 use dotenvy::dotenv;
 use info_car_api::{
@@ -12,8 +14,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let username = dotenvy::var("USERNAME")?;
     let password = dotenvy::var("PASSWORD")?;
+    let webhook_url = dotenvy::var("WEBHOOK_URL")?;
 
     let mut client = InfoCarClient::new();
+    let make_client = reqwest::Client::new();
     client.login(&username, &password).await?;
 
     let mut last_id: String = "".to_owned();
@@ -33,6 +37,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     if exam[0].id != last_id {
                         last_id = exam[0].id.clone();
                         println!("{:?}", exam);
+                        let mut map = HashMap::new();
+                        map.insert("title", "New exam available");
+                        let exam_message = format!("Date: {}", exam[0].date);
+                        map.insert("message", &exam_message);
+
+                        let res = make_client
+                            .post(&webhook_url)
+                            .json(&map)
+                            .send()
+                            .await?
+                            .text()
+                            .await?;
+
+                        println!("{res}");
                     } else {
                         println!("No change...")
                     }
